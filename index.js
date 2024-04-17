60d7783b8e2f75bfdfe7474c7c1da56cd1405520
Muhamad Abi Saleh <abi.saleh.25@gmail.com>
2024-04-17 02:57:33 +0000

    init

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let UrlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    require: true
+  },
+  short_url: {
+    type: Number
+  }
+});
+
+let UrlModel = mongoose.model("Url", UrlSchema);
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +33,41 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/:id', function(req, res) {
+  const id = req.params.id;  
+
+  UrlModel.findOne({short_url: id}, function(err, data) {
+    if (err) return console.log(err)
+    return res.redirect(data['original_url'])
+  })
+});
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+
+  UrlModel.countDocuments({}, function (err, countData) {
+    if (err) return console.log(err);
+
+    const newData = new UrlModel({
+      original_url: url,
+      short_url: countData + 1
+    })
+
+    UrlModel.findOne({original_url: url}, function(err, oldData) {
+      if (err) return console.log(err)
+
+      if (oldData) {
+        let {original_url, short_url} = oldData.toJSON();
+        return res.json({original_url, short_url});
+      }
+
+      newData.save(function(err, data) {
+        if (err) return console.log(err)
+        let {original_url, short_url} = data.toJSON();
+        res.json({original_url, short_url});
+      });
+    })
+  })
 });
 
 app.listen(port, function() {
